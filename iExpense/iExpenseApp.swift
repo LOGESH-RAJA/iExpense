8b1214f2e19b08d4524bf15c982477c9609dbe1b
Logesh R <logesh822081@gmail.com>
2024-12-03 21:44:21 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Logesh Raja on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
